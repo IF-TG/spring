fbd46403a662fd82cabd02351e553301a181089d
MoonDooo <ruddhks9937@gmail.com>
2024-02-05 18:44:45 +0900

    ,

--- travelPlan/src/main/java/ifTG/travelPlan/service/travelplan/search/machineleaning/destinationvector/DestinationVectorImpl.java
+++ travelPlan/src/main/java/ifTG/travelPlan/service/travelplan/search/machineleaning/destinationvector/DestinationVectorImpl.java
@@ -1,27 +1,38 @@
 package ifTG.travelPlan.service.travelplan.search.machineleaning.destinationvector;
 
+import ifTG.travelPlan.domain.travel.Destination;
 import ifTG.travelPlan.repository.springdata.travel.DestinationRepository;
 import ifTG.travelPlan.service.travelplan.search.machineleaning.embedding.EmbeddingModel;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @Component
 public class DestinationVectorImpl implements DestinationVector{
     private final EmbeddingModel em;
     private double[][] destinationVector;
-    private final Map<Integer, Integer> mapping;
+    private Map<Long, Long> mapping;
     private final DestinationRepository destinationRepository;
 
     public DestinationVectorImpl(@Qualifier("pvDBOW") EmbeddingModel em, DestinationRepository destinationRepository) {
         this.em = em;
         this.destinationRepository = destinationRepository;
-        mapping = new HashMap<>();
     }
 
+    public void init(){
+        List<Destination> allDestinationList = destinationRepository.findAll();
+        mapping = IntStream.range(0, allDestinationList.size()).boxed()
+                .collect(Collectors.toMap(i->i, i->allDestinationList.get(i).getId()));
 
+
+
+    }
 
 
 }
