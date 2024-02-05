72621a0a40332349d7f627520b466d4005d9bdd8
MoonDooo <ruddhks9937@gmail.com>
2024-02-05 19:27:50 +0900

    .

---
+++ travelPlan/src/main/java/ifTG/travelPlan/service/travelplan/search/machineleaning/embedding/Doc2Vec.java
@@ -0,0 +1,13 @@
+﻿package ifTG.travelPlan.service.travelplan.search.machineleaning.embedding;
+
+public class Doc2Vec implements EmbeddingModel{
+    @Override
+    public WeightBuilder learningWeight(LearningBuilder builder) {
+
+    }
+
+    @Override
+    public double[] forwardPassWithSoftmax(WeightBuilder weightBuilder, int oneHotInput) {
+
+    }
+}
