baae4fd4b56d78cefb5742f711adefd3ad40839c
MoonDooo <ruddhks9937@gmail.com>
2024-03-15 14:13:48 +0900

    refactoring

--- travelPlan/src/main/java/ifTG/travelPlan/exception/StatusCode.java
+++ travelPlan/src/main/java/ifTG/travelPlan/exception/StatusCode.java
@@ -1,4 +1,4 @@
-package ifTG.travelPlan.controller.dto;
+package ifTG.travelPlan.controller.exception;
 
 import ifTG.travelPlan.dto.StatusCodeMessageDto;
 import lombok.Getter;
@@ -30,10 +30,12 @@ public enum StatusCode {
     DUPLICATE_NICKNAME(HttpStatus.CONFLICT, "4005", "중복된 닉네임입니다."),
     AUTHENTICATION_FAILED(HttpStatus.FORBIDDEN, "4444", "잘못된 접근입니다."),
     AUTHORITY_FAILED(HttpStatus.FORBIDDEN, "4445", "권한이 없습니다."),
-    INVALID_DATE_FORMAT(HttpStatus.BAD_REQUEST, "3001", "유효하지 않은 날짜 형식입니다."),
+    INVALID_DATE_FORMAT(HttpStatus.BAD_REQUEST, "4008", "유효하지 않은 날짜 형식입니다."),
+    INVALID_IMAGE_TYPE(HttpStatus.BAD_REQUEST, "4006", "지원하지 않는 포멧입니다."),
 
     Internal_Server_Error(HttpStatus.INTERNAL_SERVER_ERROR, "5001", "죄송합니다. 요청을 처리하는 과정에서 예상치 못한 서버 오류가 발생했습니다. 문제를 빠르게 해결하기 위해 노력하고 있습니다. 문제가 지속될 경우, 이메일로 문의해 주세요."),
-    UNSUPPORTED_OAUTH2_REQUIREMENT(HttpStatus.NOT_IMPLEMENTED, "5002", "로그인 시도 중 문제가 발생하였습니다.");
+    UNSUPPORTED_OAUTH2_REQUIREMENT(HttpStatus.NOT_IMPLEMENTED, "5002", "로그인 시도 중 문제가 발생하였습니다."),
+    CANNOT_CREATE_FOLDER(HttpStatus.INTERNAL_SERVER_ERROR, "5003", "이미지 저장에 실패했습니다.");
 
 
 
